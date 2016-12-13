ef7ca36c12eec2ccd16e3bab5b577213e3cc0b89
Robert Bunch <rob@digitalcrafts.com>
2016-12-13 14:45:32 -0500

    finishe adding buttons and dom elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 // JS, wait for the DOM!
 $(document).ready(function(){
+
+Get deal working
+Need a way to make the deck
+Shuffle the new deck
+Update the DOM with the player cards
+Get hit working
+Put the card in the right place
+Update the total
+Check if the player busted
+Get stand working
+Run the dealer “hit” until it has more than 16
+Once dealer has more than 16, checkwin
+Post a message after checkwin
 	
 });
