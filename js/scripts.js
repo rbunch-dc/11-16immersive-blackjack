f4b833e26f667fd2a6aae6539fb2d2951820a225
Robert Bunch <rob@digitalcrafts.com>
2016-12-14 10:55:13 -0500

    Redo the JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,110 +1,76 @@
-// JS, wait for the DOM!
+//----------------------
+// ----GLOBALS----------
+//----------------------
+var theDeck = createDeck();
+var playersHand = [] //player1Squares in tictactoe
+var dealersHand = [] //player2Squares in tictactoe
+
 $(document).ready(function(){
-// --------------------------	
-// ---------GLOBALS----------
-// --------------------------
-	var theDeck = [];
-	var playersHand = [];
-	var dealersHand = [];
-	// Need a way to make the deck
-	createDeck();
 
-
-	// Get deal working
 	$('.deal-button').click(function(){
-		// console.log(this);
-		// Shuffle the new deck
-		shuffleDeck();
-		// Add card 0, to playersHand
+		//Deal stuff goes in here
+		shuffleDeck(); //Deck is now shuffled!
+		// Update player array and DOM
 		playersHand.push(theDeck[0]);
-		// Add card 1 to the dealersHand
-		dealersHand.push(theDeck[1]);
-		// Add card 2, to playersHand
 		playersHand.push(theDeck[2]);
-		// Add card 3, to dealersHand
+
+		placeCard('player','one',playersHand[0]);
+		placeCard('player','two',playersHand[1]);
+
+		// Update dealer array and DOM
+		dealersHand.push(theDeck[1]);
 		dealersHand.push(theDeck[3]);
 
-		// Put the first card in the players hand
-		placeCard(playersHand[0], 'player', 'one');
-		// Put the second card in the players hand
-		placeCard(playersHand[1], 'player', 'two');
+		placeCard('dealer','one',dealersHand[0]);
+		placeCard('dealer','two',dealersHand[1]);
 
-		// Put the first card in the dealers hand
-		placeCard(dealersHand[0], 'dealer', 'one');
-		// Put the first card in the dealers hand
-		placeCard(dealersHand[1], 'dealer', 'two');
-
-		calculateTotal('player',playersHand);
-		calculateTotal('dealer',dealersHand);
 
 	});
 
-
-	
-	// Update the DOM with the player cards
-	// Get hit working
-	// Get deal working
 	$('.hit-button').click(function(){
-		console.log(this);
+		//Hit stuff goes in here
 	});
 
-	// Put the card in the right place
-	// Update the total
-	// Check if the player busted
-	// Get stand working
 	$('.stand-button').click(function(){
-		console.log(this);
-	});	
-	// Run the dealer “hit” until it has more than 16
-	// Once dealer has more than 16, checkwin
-	// Post a message after checkwin
-
-	function createDeck(){
-		// Fill the deck with:
-		// - 52 cards
-		// -- 4 suits (h,s,d,c)
-		// -- 1-13 (11 = J, 12 = Q, 13 = K)
-		var suits = ['h','s','d','c'];
-		// Loop through all 4 suits (suits array)
-		for(let s = 0; s < suits.length; s++){
-			// loop through all 13 cards for each suit
-			for(let c = 1; c <= 13; c++){
-				theDeck.push(c+suits[s]);
-			}
-		}
-		console.log(theDeck);
-	}
-
-	function shuffleDeck(){
-		for(let i = 0; i < 9001; i++){
-			var card1ToSwitch = Math.floor(Math.random() * theDeck.length);
-			var card2ToSwitch = Math.floor(Math.random() * theDeck.length);
-			var temp = theDeck[card1ToSwitch];
-			theDeck[card1ToSwitch] = theDeck[card2ToSwitch]
-			theDeck[card2ToSwitch] = temp;
-			// console.log(theDeck);
-		}
-		console.log(theDeck);
-	}
-
-	function placeCard(whatCard, who, whichSlot){
-		var classToTarget = '.'+who+'-cards .card-'+whichSlot;
-		// console.log(classToTarget);
-		$(classToTarget).html('<img src="images/'+whatCard+'.png">');
-	}
-
-	function calculateTotal(who, theirHand){
-		var cardValue = 0;
-		var total = 0;
-		for(let i = 0; i < theirHand.length; i++){
-			cardValue = Number(theirHand[i].slice(0,-1));
-			console.log(cardValue);
-			total += cardValue;
-		}
-		var classToTarget = '.'+who+'-total-number';
-		$(classToTarget).text(total);
-
-
-	}
-
+		// stand stuff goes in here
+	});
 });
+
+function createDeck(){
+	var newDeck = [];
+	var suits = ['h','s','d','c'];
+	//suits/outter loop
+	for(let s = 0; s < suits.length; s++){ 
+		// card values/inner loop
+		for(let c = 1; c <= 13; c++){
+			newDeck.push(c+suits[s]);
+		}
+	}
+	return newDeck;
+}
+
+function shuffleDeck(){
+	for(let i = 0; i < 9001; i++){
+		var random1 = Math.floor(Math.random() * theDeck.length);
+		var random2 = Math.floor(Math.random() * theDeck.length);
+
+		// switch theDeck[random1] with theDeck[random2]
+		// store the value of theDeck[random1]
+		var temp = theDeck[random1];
+
+		// overwrite theDeck[random1] with theDeck[random2]
+		theDeck[random1] = theDeck[random2];
+
+		// overwrite theDeck[random2] with the temp (what used to be in random1)
+		theDeck[random2] = temp;
+	}
+	console.log(theDeck);		
+}
+
+function placeCard(who, where, whatCard){
+	var classSelector = '.' + who + '-cards .card-' + where;
+						// '.' + 'player' + '-cards .card-' + 'one'
+						// '.player-cards .card-one'
+	$(classSelector).html('<img src="images/' + whatCard + '.png">');
+	// $('.player-cards .card-one').html('<img src="cards/2d.png">')
+}
